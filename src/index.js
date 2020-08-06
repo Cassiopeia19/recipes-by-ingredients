ffe5d3559d5eb96af21dcabbf17d5f0acebd9dc9
Cassiopeia19 <cassiopeia5571@gmail.com>
2020-08-06 18:17:58 -0500

    google analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import GoogleLogin from 'react-google-login'
+
+
 
 ReactDOM.render(
+  // <GoogleLogin
+  //   clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+  //   buttonText="Login"
+  //   onSuccess={responseGoogle}
+  //   onFailure={responseGoogle}
+  //   cookiePolicy={"single_host_origin"}
+  // />,
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("App")
 );
 
 // If you want your app to work offline and load faster, you can change
