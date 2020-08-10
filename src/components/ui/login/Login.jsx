5e3bd8ac51b3bbafdc5edf9c4d839e8a7075854e
Cassiopeia19 <cassiopeia5571@gmail.com>
2020-08-10 13:35:48 -0500

    updated login & login css

--- src/components/ui/login/Login.jsx
+++ src/components/ui/login/Login.jsx
@@ -3,7 +3,15 @@ import { withRouter, Redirect } from "react-router";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import './Login.css'
-import meal from '../../../assets/images/meal.jpg'
+import recipe1 from '../../../assets/images/recipe1.jpg'
+import recipe2 from '../../../assets/images/recipe2'
+import recipe3 from '../../../assets/images/recipe3'
+import recipe4 from '../../../assets/images/recipe4'
+import recipe5 from '../../../assets/images/recipe5'
+import recipe6 from '../../../assets/images/recipe6'
+import recipe7 from '../../../assets/images/recipe7'
+import recipe8 from '../../../assets/images/recipe8'
+import recipe9 from '../../../assets/images/recipe9'
 
 class Login extends Component {
   state = { isSignedIn: false };
@@ -29,10 +37,36 @@ class Login extends Component {
   };
   render() {
     return (
-      <body
-        className="login-background"
-        style={{ backgroundImage: `url(${meal})`}}
-      >
+      <ul>
+        <li>
+        {recipe1}
+        </li>
+        <li>
+        {meal2}
+        </li>
+        <li>
+        {meal3}
+        </li>
+        <li>
+        {meal4}
+        </li>
+        <li>
+        {meal5}
+        </li>
+        <li>
+        {meal6}
+        </li>
+        <li>
+        {meal7}
+        </li>
+        <li>
+        {meal8}
+        </li>
+        </ul>
+      // <body
+      //   className="login-background"
+      //   style={{ backgroundImage: `url(${meal})`}}
+      // >
         <div>
           {this.state.isSignedIn ? (
             <span>
