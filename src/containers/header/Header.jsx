5a7fe185fdd803e37380f9e71f51f99282f006f9
Cassiopeia19 <cassiopeia5571@gmail.com>
2020-08-09 19:51:30 -0500

    updated header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,56 +1,77 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from "react";
-//import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 //import ReloadPage from "../../components/routes/ReloadPage";
 import "./Header.css";
-//import Login from '../../components/ui/login/Login'
+import Auth from "../../Auth"
 import logo from '../../assets/images/recipes.png'
 import firebase from 'firebase'
 
 class Header extends Component {
   render() {
-    // const isUserLoggedIn = Login.isSignedIn;
+    const isUserLoggedIn = Auth.AuthContext.Provider;
     return (
       <header className="header">
         <nav style={{ fontSize: "24px" }}>
           <div>
-            {/* {isUserLoggedIn && ( */}
-            <div>
-              <Link to="/home">
-                <img src={logo} alt="recipe logo" />
-              </Link>
-            </div>
-            {/* )}  */}
+            {isUserLoggedIn && (
+              <div>
+                <Link to="/home">
+                  <img src={logo} alt="recipe logo" />
+                </Link>
+              </div>
+            )}
           </div>
           <ul activeStyle={{ color: "red" }}>
-            <li>
-              <Link to="/login" onClick={withRouter}>
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
+            {isUserLoggedIn && (
+              <li>
+                <Link to="/login" onClick={withRouter}>
+                  Login
+                </Link>
+              </li>
+            )}
+            {isUserLoggedIn && (
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+            )}
+            {isUserLoggedIn && (
             <li>
               <Link to="/about">About</Link>
             </li>
+            )}
+            {isUserLoggedIn && (
             <li>
               <Link to="/recipes">Recipes</Link>
             </li>
+            )}
+            {isUserLoggedIn && (
             <li>
               <Link to="/favorites">Favorites</Link>
             </li>
+            )}
+            {isUserLoggedIn && (
             <li>
-              <Link to="/privacy-policy">
-                PrivacyPolicy
-              </Link>
+              <Link to="/privacy-policy">PrivacyPolicy</Link>
             </li>
+            )}
+            {isUserLoggedIn && (
             <li style={{ float: "right" }}>
               <Link onClick={() => firebase.auth().signOut()}>Logout</Link>
             </li>
+            )}
           </ul>
+            {!isUserLoggedIn && (
+              <li>
+                <Link
+                  to="/login"
+                  onClick={withRouter}
+                >
+                  Login
+                </Link>
+              </li>
+            )}
         </nav>
       </header>
     );
