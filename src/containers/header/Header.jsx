ac81af82ae6cc2b179ff63ff46e4be1e6795d99a
Cassiopeia19 <cassiopeia5571@gmail.com>
2020-08-09 22:14:59 -0500

    another attempt

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
               </div>
             {/* )} */}
           </div>
-          <ul className="active">
+          <ul>
             {/* {!isUserLoggedIn && ( */}
               <li>
                 <Link to="/login" onClick={withRouter}>
