5c71870273842196187e1c1dd7ad04b84d908823
Cassiopeia19 <cassiopeia5571@gmail.com>
2020-11-12 12:43:54 -0600

    added loggedin user's photo & greeting to user

--- src/components/ui/pages/home/Home.jsx
+++ src/components/ui/pages/home/Home.jsx
@@ -3,16 +3,33 @@ import homeHero from "../../../../assets/images/homeHero.jpg";
 import Hero from '../../../../Hero'
 import Banner from './banner/Banner'
 import Header from '../../../../containers/header/Header'
+import firebase from 'firebase'
 
 const Home = () => {
   // Wrap this component in a new layout with header
   return (
+    <>
     <div>
       <Header />
       <Hero img={homeHero}>
         <Banner />
       </Hero>
     </div>
+     <h1>
+        <center>
+          {" "}
+          <img
+            src={firebase.auth().currentUser.photoURL}
+            style={{
+              borderRadius: "50px",
+              height: "50px",
+              width: "50px",
+            }}
+          />
+          Welcome {firebase.auth().currentUser.displayName}!
+        </center>
+      </h1>
+      </>
   );
 }
 
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -9,34 +9,32 @@ import firebase from "firebase";
 class Header extends Component {
 
   render() {
-        return (
-          <header className="header">
-            <nav>
-              <ul>
-                <div>
-                  <Link to="/home">
-                    <img src={logo} alt="recipe logo" />
-                  </Link>
-                </div>
-                <li>
-                  <Link to="/home">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/recipes">Recipes</Link>
-                </li>
-                <li>
-                  <Link to="/favorites">Favorites</Link>
-                </li>
-                <li>
-                  <Link onClick={() => firebase.auth().signOut()}>Logout</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-        );
+    return (
+      <header className="header">
+        <nav>
+          <ul>
+            <Link to="/home">
+              <img src={logo} alt="recipe logo" />
+            </Link>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/recipes">Recipes</Link>
+            </li>
+            <li>
+              <Link to="/favorites">Favorites</Link>
+            </li>
+            <li>
+              <Link onClick={() => firebase.auth().signOut()}>Logout</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
   }
 }
 
